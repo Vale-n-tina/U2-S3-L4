1873910782ffe5c34fbbb67246803a807219882c
Vale-n-tina <valentia.grg@gmail.com>
2024-11-15 09:09:21 +0100

    .

--- pexel-start.js
+++ pexel-start.js
@@ -1,12 +1,8 @@
 const buttonLoad = document.getElementById("buttonLoad");
-const imgs = document.querySelectorAll("img");
 
-
-
-
-
-const loadFirstButton = function () {
-  fetch("https://api.pexels.com/v1/search?query=giraff&per_page=9", {
+const title=document.getElementsByClassName
+const cercacose = function (ricerca) {
+  fetch(`https://api.pexels.com/v1/search?query=${ricerca}&per_page=9`, {
     headers: {
       authorization:
         "CLAQ4dhVQGFkVWNbuioZlHGles3kNqWgzQ9C37mTnwPZyW1BL6NAzQNd ",
@@ -19,44 +15,28 @@ const loadFirstButton = function () {
         throw new Error("errore");
       }
     })
-    .then((arrayOfGiraf) => {
-      const contphoto = arrayOfGiraf.photos;
-
-      contphoto.forEach((objectphoto, index) => {
+    .then((arrayOfpig) => {
+      const arrayPhoto = arrayOfpig.photos;
+      arrayPhoto.forEach((objectphoto, index) => {
+        document.querySelectorAll("img")[index].setAttribute("src", objectphoto.src.medium);
+        document.getElementsByClassName("card-title")[index].innerText= objectphoto.alt
         
-        imgs[index].setAttribute("src", objectphoto.src.medium);
-       
       });
     })
-
     .catch((error) => {
       console.log(error);
     });
 };
 
+const loadFirstButton = function () {
+  cercacose("giraff");
+};
 
-const buttonSecondary=document.getElementById("buttonSecondary")
-buttonSecondary.addEventListener("click",()=>{
-    fetch("https://api.pexels.com/v1/search?query=pig&per_page=9", {headers: {
-        authorization:
-          "CLAQ4dhVQGFkVWNbuioZlHGles3kNqWgzQ9C37mTnwPZyW1BL6NAzQNd ",
-      },
-     })
-     .then((response)=>{
-         if(response.ok){
-             return response.json()
-            }else{
-                throw new Error ("errore")
-            }
-        })
-        .then((arrayOfpig)=>{
-         
-        const arrayPhoto=arrayOfpig.photos
-        arrayPhoto.forEach((objectphoto,index)=>{
-            imgs[index].setAttribute("src", objectphoto.src.medium)
-        })
-     })
-     .catch((error)=>{
-        console.log(error)
-     })
-})
+document.getElementById("buttonSecondary").addEventListener("click", () => {
+  cercacose("pig");
+});
+
+document.getElementById("formRicerca").addEventListener("submit", (e) => {
+  e.preventDefault();
+  cercacose(document.getElementById("ricerca").value);
+});
